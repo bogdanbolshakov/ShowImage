269aff92d30bf6c8d36b97cf13d8b674929b1ddb
bogdanbolshakov <bob_candys@mail.ru>
2021-02-18 14:51:33 +0300

    Добававлен функционал просмотра локальных изображений.

--- testCase/MainWindow.xaml.cs
+++ testCase/MainWindow.xaml.cs
@@ -1,19 +1,11 @@
-﻿using Microsoft.Win32;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows;
 using System.IO;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Forms;
 
 namespace testCase
@@ -24,32 +16,38 @@ namespace testCase
     public partial class MainWindow : Window
     {
         private List<string> picturesList = new List<string>();
+
         public MainWindow()
         {
             InitializeComponent();
         }
-        private string SelectDirWithPictures()
+
+        private string SelectDirectory()
         {
             FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
             folderBrowserDialog.ShowDialog();
             return folderBrowserDialog?.SelectedPath;
         }
-        private void SetListPictures(string path)
+
+        #region События и взаимодействие с MainWindow
+
+        private void SetTreeViewItemPictures(string path)
         {
-            if(!path.Equals(""))
+            if (!path.Equals(""))
             {
                 var dirFiles = Directory.GetFiles(path).ToList();
                 picturesList.Clear();
                 picturesList = dirFiles.FindAll(item => item.EndsWith(".jpg") || item.EndsWith(".jpeg") ||
                     item.EndsWith(".gif") || item.EndsWith(".png"));
+                TreeView_Pictures.Items.Clear();
+                picturesList.ForEach(item => AddNodeToTreeViewItemPictures(item));
+            }
+            else
+            {
+                return;
             }
         }
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            SetListPictures(SelectDirWithPictures());
-            picturesList.ForEach(item => AddNodeToTreeViewItemPictures(item));
-            TreeView_Pictures.Items.Refresh();
-        }
+
         private void AddNodeToTreeViewItemPictures(string picturePath)
         {
             var node = new TreeViewItem
@@ -62,6 +60,13 @@ namespace testCase
             TreeView_Pictures.Items.Add(node);
             node.MouseDoubleClick += Node_MouseDoubleClick;
         }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            SetTreeViewItemPictures(SelectDirectory());
+            TreeView_Pictures.Items.Refresh();
+        }
+
         private void Node_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             try
@@ -73,5 +78,7 @@ namespace testCase
                 System.Windows.Forms.MessageBox.Show("Не удалось открыть изображение.\nВозможно оно имеет неподдерживаемый формат.");
             }
         }
+
+        #endregion
     }
 }
