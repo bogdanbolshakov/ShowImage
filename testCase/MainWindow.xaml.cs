c14a5456f880c87877c3ad1d49bc9d2a0740032b
bogdanbolshakov <bob_candys@mail.ru>
2021-02-27 18:11:43 +0300

    Применение MVVM + Caliburn Micro

--- testCase/MainWindow.xaml.cs
+++ testCase/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Windows;
-using System.IO;
-using System.Windows.Controls;
-using System.Windows.Input;
-using System.Windows.Media.Imaging;
-using System.Windows.Forms;
-using System.Threading.Tasks;
+﻿using System.Windows;
+using testCase.ViewerViewModel;
 
 namespace testCase
 {
@@ -16,77 +8,10 @@ namespace testCase
     /// </summary>
     public partial class MainWindow : Window
     {
-        private List<string> picturesList = new List<string>();
-
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new ViewerVM();
         }
-
-        private string SelectDirectory()
-        {
-            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
-            folderBrowserDialog.ShowDialog();
-            return folderBrowserDialog?.SelectedPath;
-        }
-
-        #region События и взаимодействие с MainWindow
-
-        private void SetTreeViewItemPictures(string path)
-        {
-            if(!path.Equals(""))
-            {
-                Task.Factory.StartNew(
-                    () =>
-                    {
-                        Dispatcher.BeginInvoke(new Action(() =>
-                        {
-                            var dirFiles = Directory.GetFiles(path).ToList();
-                            picturesList.Clear();
-                            picturesList = dirFiles.FindAll(item => item.EndsWith(".jpg") || item.EndsWith(".jpeg") ||
-                                item.EndsWith(".gif") || item.EndsWith(".png"));
-                            TreeView_Pictures.Items.Clear();
-                            picturesList.ForEach(item => AddNodeToTreeViewItemPictures(item));
-                        }));
-                    });
-            }
-            else
-            {
-                return;
-            }
-        }
-
-        private void AddNodeToTreeViewItemPictures(string picturePath)
-        {
-            var node = new TreeViewItem
-            {
-                Header = Path.GetFileName(picturePath),
-                Tag = picturePath,
-                ToolTip = "Щелкните два раза мышкой для открытия изображения",
-                FontWeight = FontWeights.Medium
-            };
-            TreeView_Pictures.Items.Add(node);
-            node.MouseDoubleClick += Node_MouseDoubleClick;
-        }
-
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-            SetTreeViewItemPictures(SelectDirectory());
-            TreeView_Pictures.Items.Refresh();
-        }
-
-        private void Node_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            try
-            {
-                Image_Field.Source = new BitmapImage(new Uri((sender as TreeViewItem).Tag.ToString()));
-            }
-            catch
-            {
-                System.Windows.Forms.MessageBox.Show("Не удалось открыть изображение.\nВозможно оно имеет неподдерживаемый формат.");
-            }
-        }
-
-        #endregion
     }
 }
---
+++ testCase/ViewerViewModel/ViewerVM.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+using testCase.Classes;
+
+namespace testCase.ViewerViewModel
+{
+    class ViewerVM : INotifyPropertyChanged
+    {
+        private string dir;
+
+        private string imageSource;
+
+        private ObservableCollection<string> nodes;
+
+        public string Dir
+        {
+            get { return dir; }
+            set
+            {
+                dir = value;
+                OnPropertyChanged("Dir");
+            }
+        }
+
+        public string ImageSource
+        {
+            get { return imageSource; }
+            set
+            {
+                imageSource = value;
+                OnPropertyChanged("ImageSource");
+            }
+        }
+
+        public ObservableCollection<string> Nodes
+        {
+            get { return nodes; }
+            set
+            {
+                nodes = value;
+                OnPropertyChanged("Nodes");
+            }
+        }
+
+        public ViewerVM()
+        {
+            dir = string.Empty;
+            nodes = new ObservableCollection<string>();
+        }
+
+        public void SetImageSource(object item)
+        {
+            if(item.GetType().ToString().Equals("System.String"))
+            {
+                ImageSource = dir + @"\" + item.ToString();
+            }
+        }
+
+        private BindingCommand openCommand;
+
+        public BindingCommand OpenCommand
+        {
+            get
+            {
+                return openCommand ??
+                  (openCommand = new BindingCommand(obj =>
+                  {
+                      FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+                      folderBrowserDialog.ShowDialog();
+                      Dir = !folderBrowserDialog.SelectedPath.Equals("") ? folderBrowserDialog.SelectedPath : Dir;
+                      UpdateNodes();
+                  }));
+            }
+        }
+
+        private void UpdateNodes()
+        {
+            Nodes.Clear();
+            Nodes = new ObservableCollection<string>(Directory.GetFiles(Dir).
+                Select(item => item.Replace(dir + @"\", "")).ToList().
+                FindAll(item => item.EndsWith(".jpg") || item.EndsWith(".jpeg") || item.EndsWith(".gif") || item.EndsWith(".png")));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string property = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
